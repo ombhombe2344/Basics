181dd8e6fb432548b2312771a0cb2e4e88b03444
ombhombe2344 <ombhombe15@gmail.com>
2024-07-30 06:39:05 +0000

    Simple Calculator created

---
+++ simple.cpp
@@ -0,0 +1,27 @@
+// om bhombe
+//23070123039
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+   
+    float num1,num2;
+    cout << "Enter num1: ";
+    cin >> num1;
+    cout << "Enter num2: ";
+    cin >> num2;
+    cout << "The sum is: " << num1+num2 << "\n";;
+    cout << "The difference is: " << num1-num2 << "\n";;
+    cout << "The division is: " << num1/num2 << "\n";;
+    cout << "The product is: " << num1*num2 << "\n";;
+
+    return 0;
+}
+
+// Enter num1: 4
+//Enter num2: 6
+//The sum is: 10
+//The difference is: -2
+//The division is: 0.666667
+//The product is: 24
